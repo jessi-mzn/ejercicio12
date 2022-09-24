2b9ddc87bfb43792154f1f68285dbfe33b434702
jesica <113074000+jessi-mzn@users.noreply.github.com>
2022-09-24 16:45:05 -0300

    Add files via upload

---
+++ reloj.js
@@ -0,0 +1,21 @@
+
+const $tiempo = document.querySelector('.tiempo'),
+    $fecha = document.querySelector('.fecha');
+
+   function digitalClock(){
+      let f = new Date(),
+      dia = f.getDate(),
+      mes = f.getMonth(),
+      anio = f.getFullYear(),
+      diaSemana = f.getDay();
+
+   let timeString = f.toLocaleTimeString();
+      $tiempo.innerHTML = timeString;
+
+    
+
+}
+   setInterval(() => {
+      digitalClock()
+   }, 1000);
+ 
